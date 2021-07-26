3dfa306c77e26ae28444bfa11f0672152d5cb0a3
Thomas <9162301+eleccoder@users.noreply.github.com>
2021-07-26 23:52:27 +0200

    Example executable added

---
+++ src/example.c
@@ -0,0 +1,38 @@
+#include <ax25beacon.h>
+#include <wav.h>
+
+#include <stdio.h>
+
+
+static void audio_callback(void* data, int16_t* wav, size_t wav_len)
+{
+  printf("wav_len: %li\n", wav_len);
+
+  const unsigned int sample_rate_in_hz = 48000u;
+
+  WavFile* fp = wav_open("aprs.wav", WAV_OPEN_WRITE);
+
+  wav_set_format(fp, WAV_FORMAT_IEEE_FLOAT);
+  wav_set_sample_size(fp, sizeof(int16_t));
+  wav_set_num_channels(fp, 1);
+  wav_set_sample_rate(fp, sample_rate_in_hz);
+  wav_write(fp, wav, wav_len);
+
+  wav_close(fp);
+}
+
+
+int main()
+{
+  int err = ax25_beacon(NULL,              // User data
+                        audio_callback,
+                        "SRC", "DST",      // Call signs
+                        "PATH1", "PATH2",
+                        10.0, 20.0, 100.0, // Lat, lon, alt (in m)
+                        "CALL-SRC is testing ...",
+                        '/', 'O');
+
+  printf((err == 0) ? "Successful\n" : "Error\n");
+
+  return 0;
+}
